4a3536ef7fb97fc051dd1ce3645217b08c9e34c6
Marco Lamina <mlamina09@gmail.com>
2024-04-16 17:14:29 -0700

    Automatically set Github action outputs

--- pr_pilot/util.py
+++ pr_pilot/util.py
@@ -1,8 +1,10 @@
+import logging
 import os
-import time
 
 import pr_pilot
 from pr_pilot import Task, Prompt
+
+logger = logging.getLogger(__name__)
 
 
 def _get_config_from_env():
@@ -15,11 +17,30 @@ def _get_config_from_env():
     return configuration
 
 
-def create_task(repo: str, prompt: str) -> Task:
+def set_github_action_output(key: str, value: str):
+    """Set an output parameter for GitHub Actions."""
+    # Get the path to the environment file for outputs
+    output_file = os.getenv('GITHUB_OUTPUT')
+
+    # Ensure the output file path is available
+    if output_file is not None:
+        with open(output_file, "a") as file:
+            file.write(f"{key}={value}\n")
+    else:
+        logger.debug("GITHUB_OUTPUT environment variable is not set.")
+
+
+def create_task(repo: str, prompt: str, log=True) -> Task:
     """Create a task for the specified repository with the given prompt."""
     with pr_pilot.ApiClient(_get_config_from_env()) as api_client:
         api_instance = pr_pilot.TaskCreationApi(api_client)
-        return api_instance.tasks_create(Prompt(prompt=prompt, github_repo=repo))
+        task = api_instance.tasks_create(Prompt(prompt=prompt, github_repo=repo))
+        dashboard_url = f"https://app.pr-pilot.ai/dashboard/tasks/{str(task.id)}/"
+        set_github_action_output("task-id", str(task.id))
+        set_github_action_output("task-url", dashboard_url)
+        if log:
+            logger.info(f"PR Pilot task created: {dashboard_url}")
+        return task
 
 
 def get_task(task_id: str) -> Task:
